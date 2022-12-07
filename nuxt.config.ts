138b12b468794136c747494c89700d9df0e17260
Ashley Morgan <ashleymorgan19901023@gmail.com>
2022-12-07 13:13:50 -0700

    added basic nuxt content framework

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: [
+    '@nuxt/content'
+  ],
+  content: {
+    /* Added this just in case I want to override defaults */
+  },
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
