5c4513e15d3d2b0bd95d216022e0ce07a54c47f8
Myka Peligrino <mykapeligrino@gmail.com>
2022-12-08 10:52:58 -0700

    created a section header component

---
+++ components/SectionHeader.vue
@@ -0,0 +1,15 @@
+<!-- This is the page header
+TODO:
+- make it responsive and add styles with tailwind-->
+
+<script setup>
+defineProps({
+  title: String,
+});
+</script>
+
+<template>
+  <header> //insert tailwind here
+    <h1>{{ title }}</h1>
+  </header>
+</template>
