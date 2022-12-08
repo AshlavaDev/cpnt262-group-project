690c9a13df2b47978e71019fdd6d2bf9b720fd6a
Myka Peligrino <mykapeligrino@gmail.com>
2022-12-08 11:04:06 -0700

    tweaks

--- components/AppSection.vue
+++ components/AppSection.vue
@@ -1,6 +1,4 @@
-<!-- This is the scope of the <section> where the cards are contained 
-TODO:
-- add tailwind -->
+<!-- This is the scope of the <section> where the cards are contained -->
 
 <template>
   <section class="container flex flex-col gap-4">
