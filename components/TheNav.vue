e15427a15f1360f06dc55f35c413d0a4113911db
Myka Peligrino <mykapeligrino@gmail.com>
2022-12-08 09:11:08 -0700

    Update TheNav.vue

---
+++ components/TheNav.vue
@@ -0,0 +1,19 @@
+<!-- This is the footer component -->
+<!-- TODO 
+  : add tailwind css styles
+  : change Site Title
+  : change Page title 
+  : use v-for for nav bar links instead-->
+
+<template>
+  <header>
+    <nav>
+      <nuxt-link to="/">Site Title</nuxt-link>
+      <ul>
+        <li><nuxt-link to="/">Home</nuxt-link></li>
+        <li><nuxt-link to="/about">About</nuxt-link></li>
+        <li><nuxt-link to="/gallery">Gallery</nuxt-link></li>
+      </ul>
+    </nav>
+  </header>
+</template>
