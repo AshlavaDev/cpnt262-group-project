f3036136356a921f98ddf7b08dbed97c06470836
Myka Peligrino <mykapeligrino@gmail.com>
2022-12-08 11:17:49 -0700

    temporary css added

--- components/TheNav.vue
+++ components/TheNav.vue
@@ -5,13 +5,13 @@
   : change Page title -->
 
 <template>
-  <header>
-    <nav>
-      <nuxt-link to="/">Site Title</nuxt-link>
-      <ul>
+  <header class="bg-gradient-to-l from-teal-700 via-neutral-400 to-stone-600"> //this style is only temporary
+    <nav class="container mx-auto p-4 flex justify-between">
+      <nuxt-link to="/" class="font-bold lg:text-5xl">Site Title</nuxt-link>
+      <ul class="flex gap-10 text-2xl">
         <li><nuxt-link to="/">Home</nuxt-link></li>
         <li><nuxt-link to="/about">About</nuxt-link></li>
-        <li><nuxt-link to="/gallery">Gallery</nuxt-link></li>
+        <li><nuxt-link to="/gallery">Gallery</nuxt-link></li> //needs to be verified
       </ul>
     </nav>
   </header>
