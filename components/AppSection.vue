fd7a9e774a71138f99d3a736c744a1cb4eafae19
Myka Peligrino <mykapeligrino@gmail.com>
2022-12-08 11:02:35 -0700

    created a section component

---
+++ components/AppSection.vue
@@ -0,0 +1,9 @@
+<!-- This is the scope of the <section> where the cards are contained 
+TODO:
+- add tailwind -->
+
+<template>
+  <section class="container flex flex-col gap-4">
+    <slot></slot>
+  </section>
+</template>
