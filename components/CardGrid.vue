7b7114c2393c8a4454bc6dad3122a46b7e022ffd
Myka Peligrino <mykapeligrino@gmail.com>
2022-12-08 11:06:20 -0700

    created a card grid component

---
+++ components/CardGrid.vue
@@ -0,0 +1,7 @@
+<!-- This is the Card Grid component which takes care of the arrangement/behaviour of the cards inside the <section> container -->
+
+<template>
+  <div class="flex flex-wrap gap-4">
+    <slot></slot>
+  </div>
+</template>
