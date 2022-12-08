39306c1e9010f69c6ed46854a03818ea7938fc0d
Ashley Morgan <ashleymorgan19901023@gmail.com>
2022-12-08 14:28:45 -0700

    added some more styling for basic card layout, list layout, post page layout

--- components/content/Post.vue
+++ components/content/Post.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="container flex justify-center min-w-full">
+  <div class="container flex justify-center min-w-full py-4">
     <div class="flex flex-col items-center max-w-screen-sm lg:max-w-screen-lg">
       <div>
         <h2 class="text-6xl text-center py-4">{{ title }}</h2> 
         <img :src="`${image}`" :alt="`${alt}`" />
         
       </div>
-      <div class="text-center text-lg md:text-xl py-3 px-2 flex flex-col gap-2 post">
+      <div class="text-lg md:text-xl py-3 px-2 flex flex-col gap-2 post">
         <ContentSlot :use="$slots.default" />
       </div>
-      <NuxtLink to="/blog/posts">Back to Posts</NuxtLink>
+      <NuxtLink to="/blog/posts" class="px-2 py-3 text-xl bg-slate-600 text-slate-200 border-2 border-slate-600 rounded-sm hover:bg-slate-200 hover:text-slate-200">Back to Posts</NuxtLink>
     </div>
   </div>
 </template>
