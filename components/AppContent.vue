5174069ecbd9ee2f024c852a671fc61bfb6668de
Ashley Morgan <ashleymorgan19901023@gmail.com>
2022-12-09 16:35:35 -0700

    added paragraph component, about page content

---
+++ components/AppContent.vue
@@ -0,0 +1,18 @@
+<!-- A component to store written paragraph content to make formatiting easier -->
+
+<template>
+  <div>
+    <p v-for="paragraph in content" :key="paragraph.id" class="max-w-prose py-4 text-lg md:text-xl">{{ paragraph }}</p>
+  </div>
+</template>
+
+<script setup>
+
+defineProps({
+  content: {
+    type: Array,
+    default: ['Website paragraph content']
+  }
+})
+
+</script>
--- pages/about.vue
+++ pages/about.vue
@@ -1,11 +1,18 @@
 <template>
   <SectionHeader :title="pageTitle" />
-  <AppMain />
+  <AppMain>
+    <AppContent :content="aboutContent" />
+  </AppMain>
   
 </template>
 
 <script setup>
 
 const pageTitle = "About Us";
+
+const aboutContent = [
+  "At Labyritronics, we are all about robots, computers, and artificial intelligence. Whether it is exploring the potential uses of emerging technologies, or looking at how those technologies can impact society, we take an interest in what is being developed.",
+  "Through our blog, read our musing on how the technology is being used in public. The authors, sometimes with some help from the very technology they write about, discuss their findings on artificial intelligence and its ramifications both good and bad."
+]
 
 </script>
