1b14c7eddef17d524191d9697f523c300747400b
Myka Peligrino <mykapeligrino@gmail.com>
2022-12-09 18:46:57 -0700

    footer update

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -3,6 +3,6 @@
 
 <template>
   <footer>
-    <p class="grid h-20 place-items-center bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-gray-900 via-slate-600 to-violet-900 text-2xl mt-auto">&copy; 2022 Ashley Morgan &amp; Myka Peligrino</p>
+    <p class="grid h-20 place-items-center bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-gray-900 via-slate-600 to-violet-900 text-xl text-slate-300 mt-auto">&copy; 2022 Ashley Morgan &amp; Myka Peligrino</p>
   </footer>
 </template>bg-gradient-to-r from-violet-500 to-slate-500
