c40927ac1ae4835e55fe00a08aa5a1d3a279d51e
Ashley Morgan <ashleymorgan19901023@gmail.com>
2022-12-09 19:52:42 -0700

    fixed blog post images for size, made header h1 have text centre

--- components/SectionHeader.vue
+++ components/SectionHeader.vue
@@ -4,7 +4,7 @@ TODO:
 
 <template>
   <header class="flex flex-col justify-center items-center grow p-5 m-auto min-w-full bg-cover bg-no-repeat bg-center font-headerDisplay">
-    <h1>{{ title }}</h1>
+    <h1 class="text-center">{{ title }}</h1>
     <slot></slot>     
   </header>
 </template>
--- components/content/Post.vue
+++ components/content/Post.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container flex justify-center min-w-full py-4">
     <div class="flex flex-col items-center max-w-screen-sm lg:max-w-screen-lg">
-      <div>
+      <div class="flex flex-col items-center">
         <SectionHeader :title="title" class="text-4xl md:text-6xl"/>
-        <img :src="`${image}`" :alt="`${alt}`" />
+        <img :src="`${image}`" :alt="`${alt}`" loading="lazy" class="block object-scale-down  max-w-sm"/>
       </div>
       <div class="text-lg md:text-xl py-3 px-2 flex flex-col gap-2 post font-contentSans">
         <ContentSlot :use="$slots.default" />
